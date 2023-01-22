2dfffd3113e5e42ae6d7e4a0471983fa436e1ace
Zihan Chen <me@zihan.ga>
2023-01-23 07:56:14 +0800

    Fix ~/config not found

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config"
 import solidJs from "@astrojs/solid-js"
 import sitemap from "@astrojs/sitemap"
-import { SITE_URL } from "~/config"
+import { SITE_URL } from "./src/config"
 
 // https://astro.build/config
 export default defineConfig({
